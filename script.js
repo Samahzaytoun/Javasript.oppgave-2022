ba9ef6fa2147cd85710f7c5770fc64f47aefd98f
samah zaytoun <93342837+Samahzaytoun@users.noreply.github.com>
2022-01-10 14:01:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let numberone = 10;
+let numbertwo = 2;
+
+if(numberone=numbertwo){
+    console.log("true")
+}
+
+var value1 = 20;
+var value2 = 2;
+var numsum =( value1 > value2);
+console.log(numsum)
+
+let stringanimals = "Dog";
+console.log(stringanimals)
+
+let myname = "Samah";
+let lastname = "Zaytoun";
+if("true"){
+    console.log(myname,lastname);
+}
+
+let frog1 = "Frog"
+if("true"){
+    console.log(frog1)
+}
+
+var orderhasshipped = "false";
+if(orderhasshipped="true"){
+    console.log( "The order shipped");
+}
+else{
+    console.log("The order did not shipped");
+}
+
+for(i=7; i < 13; i++){
+    console.log(i)
+}
